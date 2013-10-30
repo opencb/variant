b6361d944998b59cbac411fb2e23d9a3a7fc2162
Alejandro Alemán <aaleman@cipf.es>
2013-10-30 14:25:28 +0100

    VCFEFFECT: Added the new library for effect calculations

--- variant-lib/src/main/java/org/opencb/variant/lib/runners/VariantEffectRunner.java
+++ variant-lib/src/main/java/org/opencb/variant/lib/runners/VariantEffectRunner.java
@@ -1,17 +1,12 @@
 package org.opencb.variant.lib.runners;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.multipart.FormDataMultiPart;
 import org.opencb.commons.bioformats.variant.vcf4.VariantEffect;
 import org.opencb.commons.bioformats.variant.vcf4.VcfRecord;
+import org.opencb.commons.bioformats.variant.vcf4.effect.EffectCalculator;
 import org.opencb.commons.bioformats.variant.vcf4.io.readers.VariantDataReader;
 import org.opencb.commons.bioformats.variant.vcf4.io.writers.effect.VariantEffectDataWriter;
 
-import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -34,47 +29,11 @@ public class VariantEffectRunner extends VariantRunner {
     public List<VcfRecord> apply(List<VcfRecord> batch) throws IOException {
 
         if (writer != null) {
-            List<VariantEffect> batchEffect = this.variantEffects(batch);
+            List<VariantEffect> batchEffect = EffectCalculator.getEffects(batch);
             ((VariantEffectDataWriter) writer).writeVariantEffect(batchEffect);
         }
 
         return batch;
     }
 
-    private List<VariantEffect> variantEffects(List<VcfRecord> batch) {
-
-
-        ObjectMapper mapper = new ObjectMapper();
-        List<VariantEffect> batchEffect = new ArrayList<>();
-
-        StringBuilder chunkVcfRecords = new StringBuilder();
-
-        Client wsRestClient = Client.create();
-        WebResource webResource = wsRestClient.resource("http://ws.bioinfo.cipf.es/cellbase/rest/latest/hsa/genomic/variant/");
-
-        for (VcfRecord record : batch) {
-            chunkVcfRecords.append(record.getChromosome()).append(":");
-            chunkVcfRecords.append(record.getPosition()).append(":");
-            chunkVcfRecords.append(record.getReference()).append(":");
-            chunkVcfRecords.append(record.getAlternate()).append(",");
-
-        }
-
-        FormDataMultiPart formDataMultiPart = new FormDataMultiPart();
-        formDataMultiPart.field("variants", chunkVcfRecords.substring(0, chunkVcfRecords.length() - 1));
-
-        String response = webResource.path("consequence_type").queryParam("of", "json").type(MediaType.MULTIPART_FORM_DATA).post(String.class, formDataMultiPart);
-
-        try {
-            batchEffect = mapper.readValue(response, mapper.getTypeFactory().constructCollectionType(List.class, VariantEffect.class));
-        } catch (IOException e) {
-            System.err.println(chunkVcfRecords.toString());
-            e.printStackTrace();
-        }
-
-
-
-
-        return batchEffect;
-    }
 }
