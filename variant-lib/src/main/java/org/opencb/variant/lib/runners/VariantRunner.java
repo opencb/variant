87ba18d2f96fd7a601f387c31e76b091141b6c3b
Alejandro Alemán <aaleman@cipf.es>
2014-02-10 12:11:54 +0100

    Renamed PedReader mentions

--- variant-lib/src/main/java/org/opencb/variant/lib/runners/VariantRunner.java
+++ variant-lib/src/main/java/org/opencb/variant/lib/runners/VariantRunner.java
@@ -1,16 +1,13 @@
 package org.opencb.variant.lib.runners;
 
-import org.opencb.commons.bioformats.pedigree.io.readers.PedDataReader;
+import org.opencb.commons.bioformats.pedigree.io.readers.PedigreeReader;
 import org.opencb.commons.bioformats.variant.Variant;
 import org.opencb.commons.bioformats.variant.VariantStudy;
-import org.opencb.commons.bioformats.variant.vcf4.VcfRecord;
 import org.opencb.commons.bioformats.variant.vcf4.io.readers.VariantReader;
 import org.opencb.commons.bioformats.variant.vcf4.io.writers.VariantWriter;
-import org.opencb.commons.io.DataWriter;
 import org.opencb.commons.run.Runner;
 import org.opencb.commons.run.Task;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -24,13 +21,13 @@ public class VariantRunner extends Runner<Variant> {
 
     protected VariantStudy study;
 
-    public VariantRunner(VariantStudy study, VariantReader reader, PedDataReader pedReader, List<VariantWriter> writer, List<Task<Variant>> tasks) {
+    public VariantRunner(VariantStudy study, VariantReader reader, PedigreeReader pedReader, List<VariantWriter> writer, List<Task<Variant>> tasks) {
         super(reader, writer, tasks);
         this.study = study;
         parsePhenotypes(pedReader);
     }
 
-    private void parsePhenotypes(PedDataReader pedReader) {
+    private void parsePhenotypes(PedigreeReader pedReader) {
         if (pedReader != null) {
             pedReader.open();
             study.setPedigree(pedReader.read());
