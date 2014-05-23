dd6158ad1d073f9ae88829742e6e8b0a9540ffd1
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2014-05-23 14:44:51 +0100

    Using new version of VariantSource class

--- variant-cli/src/main/java/org/opencb/variant/cli/VariantMain.java
+++ variant-cli/src/main/java/org/opencb/variant/cli/VariantMain.java
@@ -138,8 +138,8 @@ public class VariantMain {
         List<Task<Variant>> taskList = new SortedList<>();
         List<VariantWriter> writers = new ArrayList<>();
 
-        VariantSource study = new VariantSource(inputFile, "s1", "study1", "Study 1", Arrays.asList("Alejandro", "Cristina"), Arrays.asList(inputFile, pedFile));
-        VariantReader reader = new VariantVcfReader(inputFile, "s1", "s1");
+        VariantSource study = new VariantSource(inputFile, "f1", "s1", "Study 1");
+        VariantReader reader = new VariantVcfReader(study, inputFile);
 //        VariantWriter writer = new VariantVcfSqliteWriter(outputFile);
         List<VariantFilter> filters = parseFilters(commandLine);
         List<VariantAnnotator> annots = parseAnnotations(commandLine);
