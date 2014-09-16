dab8695c346129d8b3f73d6af7537415afe86b3b
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2014-09-16 10:14:55 +0100

    Stats calculation depending on the aggregation of the input file

--- variant-lib/src/main/java/org/opencb/variant/lib/runners/tasks/VariantStatsTask.java
+++ variant-lib/src/main/java/org/opencb/variant/lib/runners/tasks/VariantStatsTask.java
@@ -3,9 +3,11 @@ package org.opencb.variant.lib.runners.tasks;
 import java.io.IOException;
 import java.util.List;
 import org.opencb.biodata.formats.variant.io.VariantReader;
+import org.opencb.biodata.models.variant.ArchivedVariantFile;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.VariantSource;
 import org.opencb.biodata.models.variant.stats.ArchivedVariantFileStats;
+import org.opencb.biodata.models.variant.stats.VariantAggregatedStats;
 import org.opencb.biodata.models.variant.stats.VariantStats;
 import org.opencb.commons.run.Task;
 
@@ -35,7 +37,24 @@ public class VariantStatsTask extends Task<Variant> {
 
     @Override
     public boolean apply(List<Variant> batch) throws IOException {
-        VariantStats.calculateStatsForVariantsList(batch, study.getPedigree());
+//        VariantStats.calculateStatsForVariantsList(batch, study.getPedigree());
+        for (Variant variant : batch) {
+            for (ArchivedVariantFile file : variant.getFiles().values()) {
+                VariantStats variantStats = null;
+                switch (study.getAggregation()) {
+                    case NONE:
+                        variantStats = new VariantStats(variant);
+                        break;
+                    case BASIC:
+                        variantStats = new VariantAggregatedStats(variant);
+                        break;
+                    case EVS:
+                        break;
+                }
+                file.setStats(variantStats.calculate(file.getSamplesData(), file.getAttributes(), study.getPedigree()));
+            }
+        }
+        
         stats.updateFileStats(batch);
         stats.updateSampleStats(batch, study.getPedigree());
         return true;
