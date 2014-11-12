0403cdee164e3c29e8c4eca50190db1ac4c2a5b2
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2014-11-12 14:14:08 +0000

    biodata ArchivedVariantFile and ArchivedVariantFileStats classes renamed to VariantSourceEntry and VariantSourceStats

--- variant-lib/src/main/java/org/opencb/variant/lib/runners/tasks/VariantStatsTask.java
+++ variant-lib/src/main/java/org/opencb/variant/lib/runners/tasks/VariantStatsTask.java
@@ -3,10 +3,10 @@ package org.opencb.variant.lib.runners.tasks;
 import java.io.IOException;
 import java.util.List;
 import org.opencb.biodata.formats.variant.io.VariantReader;
-import org.opencb.biodata.models.variant.ArchivedVariantFile;
+import org.opencb.biodata.models.variant.VariantSourceEntry;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.VariantSource;
-import org.opencb.biodata.models.variant.stats.ArchivedVariantFileStats;
+import org.opencb.biodata.models.variant.stats.VariantSourceStats;
 import org.opencb.biodata.models.variant.stats.VariantAggregatedStats;
 import org.opencb.biodata.models.variant.stats.VariantStats;
 import org.opencb.commons.run.Task;
@@ -18,30 +18,30 @@ import org.opencb.commons.run.Task;
 public class VariantStatsTask extends Task<Variant> {
 
     private VariantReader reader;
-    private VariantSource study;
-    private ArchivedVariantFileStats stats;
+    private VariantSource source;
+    private VariantSourceStats stats;
 
     public VariantStatsTask(VariantReader reader, VariantSource study) {
         super();
         this.reader = reader;
-        this.study = study;
-        stats = new ArchivedVariantFileStats(study.getFileId(), study.getStudyId());
+        this.source = study;
+        stats = new VariantSourceStats(study.getFileId(), study.getStudyId());
     }
 
     public VariantStatsTask(VariantReader reader, VariantSource study, int priority) {
         super(priority);
         this.reader = reader;
-        this.study = study;
-        stats = new ArchivedVariantFileStats(study.getFileId(), study.getStudyId());
+        this.source = study;
+        stats = new VariantSourceStats(study.getFileId(), study.getStudyId());
     }
 
     @Override
     public boolean apply(List<Variant> batch) throws IOException {
-//        VariantStats.calculateStatsForVariantsList(batch, study.getPedigree());
+//        VariantStats.calculateStatsForVariantsList(batch, source.getPedigree());
         for (Variant variant : batch) {
-            for (ArchivedVariantFile file : variant.getFiles().values()) {
+            for (VariantSourceEntry file : variant.getSourceEntries().values()) {
                 VariantStats variantStats = null;
-                switch (study.getAggregation()) {
+                switch (source.getAggregation()) {
                     case NONE:
                         variantStats = new VariantStats(variant);
                         break;
@@ -49,20 +49,21 @@ public class VariantStatsTask extends Task<Variant> {
                         variantStats = new VariantAggregatedStats(variant);
                         break;
                     case EVS:
+                        // TODO Should create an object!
                         break;
                 }
-                file.setStats(variantStats.calculate(file.getSamplesData(), file.getAttributes(), study.getPedigree()));
+                file.setStats(variantStats.calculate(file.getSamplesData(), file.getAttributes(), source.getPedigree()));
             }
         }
         
         stats.updateFileStats(batch);
-        stats.updateSampleStats(batch, study.getPedigree());
+        stats.updateSampleStats(batch, source.getPedigree());
         return true;
     }
 
     @Override
     public boolean post() {
-        study.setStats(stats.getFileStats());
+        source.setStats(stats.getFileStats());
         return true;
     }
 }
